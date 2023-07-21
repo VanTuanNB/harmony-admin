eef8477f9bf2749387b9c0b1950fbdfc71bc1a5c
VanTuanNB <vantuan26072002@gmail.com>
2023-07-21 21:48:00 +0700

    generate source admin

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,37 @@
+import BarChartBox from "../../components/barChartBox/BarChartBox";
+import BigChartBox from "../../components/bigChartBox/BigChartBox";
+import ChartBox from "../../components/chartBox/ChartBox";
+import PieChartBox from "../../components/pieCartBox/PieChartBox";
+import TopBox from "../../components/topBox/TopBox";
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4">
+        <PieChartBox />
+      </div>
+      
+      <div className="box box7">
+        <BigChartBox />
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
