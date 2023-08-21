903d1ebfc3d4b2fbfd90594d57a9cf7620a31bd7
tienbach123 <phantienbach123@gmail.com>
2023-08-21 23:05:45 +0700

    [HMA-03] create layout login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,21 @@
+import "./Login.scss";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login-box">
+      <h2 className="title">Đăng nhập</h2>
+      <form action="">
+        <div className="user-box">
+          <input type="text" required />
+          <label htmlFor="">Email</label>
+        </div>
+        <div className="user-box">
+          <input type="password" required />
+          <label htmlFor="">Password</label>
+        </div>
+        <button className="button">Đăng nhập</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
